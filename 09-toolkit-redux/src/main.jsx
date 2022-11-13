776e0ef9266981853e69b9c56feab0aa84c0aca0
Joel Dominguez <joel.dominguez@s2g.energy>
2022-11-13 16:58:40 -0600

    Fin seccion 18

---
+++ 09-toolkit-redux/src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import App from './App'
+import './index.css'
+import { PokemonApp } from './PokemonApp'
+import { store } from './store'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <React.StrictMode>
+      {/* <App /> */}
+      <PokemonApp/>
+    </React.StrictMode>
+  </Provider>
+)
