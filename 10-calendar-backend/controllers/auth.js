14043349c1bda23f6637ba058fd2a1427bc1c2a1
devJ03L <joel.dev.net@gmail.com>
2022-12-09 18:14:23 -0600

    Backend con NodeJS

---
+++ 10-calendar-backend/controllers/auth.js
@@ -0,0 +1,48 @@
+const { response } = require('express')
+const Usuario = require('../models/Usuario')
+
+const crearUsuario = async (req, res = response) => {
+
+    //const { name, email, password } = req.body
+    try {
+        const usuario = new Usuario(req.body)
+
+        await usuario.save()
+
+        res.status(201).json({
+            ok: true,
+            msg: 'registro'
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Comuniquese con el administrador'
+        })
+    }
+}
+
+const loginUsuario = (req, res = response) => {
+
+    const { email, password } = req.body
+
+    res.json({
+        ok: true,
+        msg: 'login',
+        email,
+        password
+    })
+}
+
+const revalidarToken = (req, res = response) => {
+    res.json({
+        ok: true,
+        msg: 'renew'
+    })
+}
+
+module.exports = {
+    crearUsuario,
+    loginUsuario,
+    revalidarToken
+}
---
+++ 10-calendar-backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+require('dotenv').config()
+const { dbConnection } = require('./database/config')
+
+//Crear el servidor de express
+const app = express()
+
+//Base de datos
+dbConnection()
+
+//Directorio publico
+app.use(express.static('public'))
+
+//Lectura y parseo del body
+app.use(express.json())
+
+//Rutas
+app.use('/api/auth', require('./routes/auth'))
+//CRUD: Eventos
+
+//Escuchar peticiones
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
+})
