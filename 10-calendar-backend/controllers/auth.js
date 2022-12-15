76f7ae4d07987b1bd0e6e591cc047d55a8cd9f60
devJ03L <joel.dev.net@gmail.com>
2022-12-14 18:13:36 -0600

    Fin login en backend

--- 10-calendar-backend/controllers/auth.js
+++ 10-calendar-backend/controllers/auth.js
@@ -1,17 +1,38 @@
 const { response } = require('express')
 const Usuario = require('../models/Usuario')
+const bcrypt = require('bcryptjs')
+const { generarJWT } = require('../helpers/jwt')
 
 const crearUsuario = async (req, res = response) => {
 
-    //const { name, email, password } = req.body
+    const { name, email, password } = req.body
     try {
-        const usuario = new Usuario(req.body)
+
+        let usuario = await Usuario.findOne({ email })
+
+        if (usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Un usuario existe con ese correo'
+            })
+        }
+
+        usuario = new Usuario(req.body)
+
+        //Encriptar contraseña
+        const salt = bcrypt.genSaltSync()
+        usuario.password = bcrypt.hashSync(password, salt)
 
         await usuario.save()
 
+        //Generar JWT
+        const token = await generarJWT(usuario.id, usuario.name)
+
         res.status(201).json({
             ok: true,
-            msg: 'registro'
+            uid: usuario.id,
+            name: usuario.name,
+            token
         })
     } catch (error) {
         console.log(error)
@@ -22,22 +43,59 @@ const crearUsuario = async (req, res = response) => {
     }
 }
 
-const loginUsuario = (req, res = response) => {
+const loginUsuario = async (req, res = response) => {
 
     const { email, password } = req.body
 
-    res.json({
-        ok: true,
-        msg: 'login',
-        email,
-        password
-    })
+    try {
+        let usuario = await Usuario.findOne({ email })
+
+        if (!usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario no existe'
+            })
+        }
+
+        //Confirmar passwords
+        const validPassword = bcrypt.compareSync(password, usuario.password)
+
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Password incorrecto'
+            })
+        }
+
+        //Generar JWT        
+        const token = await generarJWT(usuario.id, usuario.name)
+
+        res.status(200).json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name,
+            token
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Comuniquese con el administrador'
+        })
+    }
 }
 
-const revalidarToken = (req, res = response) => {
+const revalidarToken = async (req, res = response) => {
+
+    const {uid, name} = req
+
+    //generar nuevo JWT
+    const token = await generarJWT(uid, name)
+
     res.json({
-        ok: true,
-        msg: 'renew'
+        ok: true,        
+        token
     })
 }
 
--- 10-calendar-backend/index.js
+++ 10-calendar-backend/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 require('dotenv').config()
 const { dbConnection } = require('./database/config')
+const cors = require('cors')
 
 //Crear el servidor de express
 const app = express()
 
 //Base de datos
 dbConnection()
+
+//CORS
+app.use(cors())
 
 //Directorio publico
 app.use(express.static('public'))
